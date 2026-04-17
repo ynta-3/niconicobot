c64ddc88b6190f170008b4198252910f4b4d11c3
ynta-3 <dashou47@gmail.com>
2026-04-17 12:51:44 +0900

    Use env var for token and fix MessageAPI call

---
+++ go.mod
@@ -0,0 +1,15 @@
+module niconicobot
+
+go 1.25.3
+
+require (
+	github.com/traPtitech/go-traq v0.0.0-20260305130843-fd44d800c38e
+	github.com/traPtitech/traq-ws-bot v1.2.1
+)
+
+require (
+	github.com/gofrs/uuid/v5 v5.3.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	golang.org/x/oauth2 v0.22.0 // indirect
+	gopkg.in/validator.v2 v2.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,37 +1,43 @@
 package main
 
-import (        
-        "context"
-        "log"
+import (
+	"context"
+	"log"
+	"os"
 
-        traq "github.com/traPtitech/go-traq"
-        traqwsbot "github.com/traPtitech/traq-ws-bot"
-        payload "github.com/traPtitech/traq-ws-bot/payload"
+	traq "github.com/traPtitech/go-traq"
+	traqwsbot "github.com/traPtitech/traq-ws-bot"
+	payload "github.com/traPtitech/traq-ws-bot/payload"
 )
 
 func main() {
-        bot, err := traqwsbot.NewBot(&traqwsbot.Options{
-                AccessToken: "CYclBElpFyELEvoeTehdHWkAKioLXiATzSSs",
-        })
-        if err != nil {
-                panic(err)
-        }
+	accessToken := os.Getenv("TRAQ_ACCESS_TOKEN")
+	if accessToken == "" {
+		log.Fatal("TRAQ_ACCESS_TOKEN is not set")
+	}
 
-        bot.OnMessageCreated(func(p *payload.MessageCreated) {
-                log.Println("Received MESSAGE_CREATED event: " + p.Message.Text)
-                _, _, err := bot.API().
-                        MessageApi.
-                        PostMessage(context.Background(), p.Message.ChannelID).
-                        PostMessageRequest(traq.PostMessageRequest{
-                                Content: "oisu-",
-                        }).
-                        Execute()
-                if err != nil {
-                        log.Println(err)
-                }
-        })
+	bot, err := traqwsbot.NewBot(&traqwsbot.Options{
+		AccessToken: accessToken,
+	})
+	if err != nil {
+		panic(err)
+	}
 
-        if err := bot.Start(); err != nil {
-                panic(err)
-        }
+	bot.OnMessageCreated(func(p *payload.MessageCreated) {
+		log.Println("Received MESSAGE_CREATED event: " + p.Message.Text)
+		_, _, err := bot.API().
+			MessageAPI.
+			PostMessage(context.Background(), p.Message.ChannelID).
+			PostMessageRequest(traq.PostMessageRequest{
+				Content: "oisu-",
+			}).
+			Execute()
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
+	if err := bot.Start(); err != nil {
+		panic(err)
+	}
 }
